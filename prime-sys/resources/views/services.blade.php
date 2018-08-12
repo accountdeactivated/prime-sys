d0b807cf9ee4512b14311da3f48442791f9de901
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-12 14:26:15 +0800

    update

--- prime-sys/resources/views/services.blade.php
+++ prime-sys/resources/views/services.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <meta charset="utf-8" />
-      <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-      <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      <title>Prime Auto Restoration</title>
-      <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-      <link href={{asset('css/material-dashboard.css?v=2.1.0')}} rel="stylesheet" />
-      <link href={{asset('demo/demo.css')}} rel="stylesheet" />
+        <meta charset="utf-8" />
+        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Prime Auto Restoration</title>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+        <link href={{asset('css/material-dashboard.css?v=2.1.0')}} rel="stylesheet" />
+        <link href={{asset('demo/demo.css')}} rel="stylesheet" />
+        {{-- <link type="text/css" rel="stylesheet" href={{asset('materialize/css/materialize.min.css')}} media="screen,projection"/> --}}
    </head>
    <body class="">
       <div class="wrapper ">
@@ -20,9 +21,19 @@
         commented by: PrivateAirJET
         -->
       <div id="addNewServiceModal" class="modal">
+        <div class="modal-header">
+            <h4>Modal Header</h4>
+            <p>A bunch of text</p>
+        </div>
+
         <div class="modal-content">
           <h4>Modal Header</h4>
           <p>A bunch of text</p>
+        </div>
+
+        <div class="modal-footer">
+            <h4>Modal Header</h4>
+            <p>A bunch of text</p>
         </div>
       </div>
        <!--modal_ends_here 
@@ -81,11 +92,12 @@
       <script src={{asset('js/plugins/bootstrap-notify.js')}}></script>
       <script src={{asset('js/material-dashboard.min.js?v=2.1.0')}}></script>
       <script src={{asset('demo/demo.js')}}></script>
+      {{-- <script type="text/javascript" src={{asset('materialize/js/materialize.min.js')}}></script> --}}
 
       <!--start custom script section-->
       <script type="text/javascript">
         $(document).on('click','#addNewServiceButton',function(){
-            $('#addNewServiceModal').modal(); 
+            $('#addNewServiceModal').modal('open'); 
         });
       </script>
       <!--end custom script section-->
