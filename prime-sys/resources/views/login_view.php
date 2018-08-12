70892a50f77daed15444c02dfab020f9a19b9bdd
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-12 13:15:38 +0800

    update login

--- prime-sys/resources/views/login_view.php
+++ prime-sys/resources/views/login_view.php
@@ -8,18 +8,14 @@
     Welcome to Prime Auto Repair
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- CSS Files -->
   <link href="../assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="../assets/demo/demo.css" rel="stylesheet" />
 </head>
     
     <body background="../assets/img/el-alce-web-379807-unsplash.jpg" style="background-attachment: fixed; background-repeat:no-repeat;
    background-size:cover;">
-        <form action="<?= site_url('login') ?>" method="post">
         
             
         <span class="align-middle">
@@ -42,7 +38,6 @@
                 <div class="card-body">
                 <br>
                 <h2 class="card-title">Prime Auto</h2>
-                    <?php if(isset($_SESSION)) {echo $this->session->flashdata('flash_data');}?>
                     <div class="form-group">
                         <input name="userName" type="text" class="form-control" autocomplete="off" placeholder="Username" required/> <br><br>
                         <input name="password" type="password" class="form-control" placeholder="Passsword" required/> <br><br>
@@ -58,9 +53,5 @@
             
         </div>
     </span>
-        
-
-        
-        </form>
     </body>
 </html>
