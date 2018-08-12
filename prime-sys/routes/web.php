956a862bf4f5bc9180e9c39e2657d7950d35d41e
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-12 19:26:40 +0800

    Updated as of 7PM - PH time

--- prime-sys/routes/web.php
+++ prime-sys/routes/web.php
@@ -13,14 +13,34 @@
 
 //Prime System Web Application Routes 
 //Done By: PrivateAirJET
-Route::get('/', function () {return view('login_view');});
-Route::get('/dashboard', function () {return view('dashboard');});
-Route::get('/service', function () {return view('service');});
-Route::get('/material', function () {return view('material');});
-Route::get('/client', function () {return view('client');});
-Route::get('/user', function () {return view('user');});
-Route::get('/car', function () {return view('car');});
-Route::get('/carmaker', function () {return view('carmaker');});
+
+//Reference Implementation of the Core Data
+// Route::get('/', function () {return view('login_view');});
+// Route::get('/dashboard', function () {return view('dashboard');});
+// Route::get('/service', function () {return view('service');});
+// Route::get('/material', function () {return view('material');});
+// Route::get('/client', function () {return view('client');});
+// Route::get('/user', function () {return view('user');});
+// Route::get('/car', function () {return view('car');});
+// Route::get('/carmaker', function () {return view('carmaker');});
+
+//Implementation of Resource Routes
+//Done By: PrivateAirJET
+Route::resource('service', 'ServicesController');
+
+//Live AJAX Update
+//Done By: PrivateAirJET
+Route::post('liveAddService','ServiceAddLiveUpdateController@liveUpdate');
+Route::post('liveEditService','ServiceEditLiveUpdateController@liveUpdate');
 
 
 
+
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
