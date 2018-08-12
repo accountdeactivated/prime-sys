d1400973ca2cc8cd0731e49300bfa334b45fcd8c
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-12 14:45:55 +0800

    modal service

--- prime-sys/resources/views/services.blade.php
+++ prime-sys/resources/views/services.blade.php
@@ -9,7 +9,7 @@
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-        <link href={{asset('css/material-dashboard.css?v=2.1.0')}} rel="stylesheet" />
+        <link href={{asset('css/material-dashboard.css')}} rel="stylesheet" />
         <link href={{asset('demo/demo.css')}} rel="stylesheet" />
         {{-- <link type="text/css" rel="stylesheet" href={{asset('materialize/css/materialize.min.css')}} media="screen,projection"/> --}}
    </head>
@@ -20,22 +20,51 @@
         <!--modal_starts_here 
         commented by: PrivateAirJET
         -->
-      <div id="addNewServiceModal" class="modal">
-        <div class="modal-header">
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
-        </div>
+        <div id="addNewServiceModal" class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add New Service</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" style="color:black;">
 
-        <div class="modal-content">
-          <h4>Modal Header</h4>
-          <p>A bunch of text</p>
-        </div>
+                    <!--service_name-->
+                    <label for="serviceName">Service Name</label>
+                    <input type="email" class="form-control" id="serviceName" placeholder="Enter the service name">
+                    <span class="text-muted" style="font-size:12px; color:black; font-family:Helvetica,Arial,sans-serif;">Note: Enter the name of the service. <b style="color:#E53935;">*Required</b></span>
 
-        <div class="modal-footer">
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
+                    <!--service_type-->
+                    <label for="serviceType">Service Type</label>
+                    <select class="form-control selectpicker" data-style="btn btn-link" id="serviceType">
+                        <option selected disabled>Choose a service type</option>
+                        <option>Repair</option>
+                        <option>Painting</option>
+                        <option>Washing</option>
+                    </select>
+                    <span class="text-muted" style="font-size:12px; color:black; font-family:Helvetica,Arial,sans-serif;">Note: Enter the type of the service. <b style="color:#E53935;">*Required</b></span>
+
+                    <!--service_description-->
+                    <div class="form-group">
+                        <label for="serviceDescription">Service Description</label>
+                        <textarea class="form-control" id="serviceDescription" rows="3"></textarea>
+                    </div>
+                    <span class="text-muted" style="font-size:12px; color:black; font-family:Helvetica,Arial,sans-serif;">Note: Enter the brief description of the service. <b style="color:#E53935;">*Required</b></span>
+
+                    <!--service_cost-->
+                    <label for="serviceCost">Service Cost</label>
+                    <input type="number" class="form-control" id="serviceCost" placeholder="Enter the service cost.">
+                    <span class="text-muted" style="font-size:12px; color:black; font-family:Helvetica,Arial,sans-serif;">Note: Enter the cost of the service. <b style="color:#E53935;">*Required</b></span>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button id="submitAddService"type="button" class="btn btn-primary">Submit</button>
+                </div>
+                </div>
+            </div>
         </div>
-      </div>
        <!--modal_ends_here 
         commented by: PrivateAirJET
         -->
@@ -90,7 +119,7 @@
       <script src={{asset('js/plugins/perfect-scrollbar.jquery.min.js')}}></script>
       <script src={{asset('js/plugins/chartist.min.js')}}></script>
       <script src={{asset('js/plugins/bootstrap-notify.js')}}></script>
-      <script src={{asset('js/material-dashboard.min.js?v=2.1.0')}}></script>
+      <script src={{asset('js/material-dashboard.min.js')}}></script>
       <script src={{asset('demo/demo.js')}}></script>
       {{-- <script type="text/javascript" src={{asset('materialize/js/materialize.min.js')}}></script> --}}
 
