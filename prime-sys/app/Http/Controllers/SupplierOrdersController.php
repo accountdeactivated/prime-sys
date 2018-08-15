23a6e6367557623157468f1151fbba51cd252ecb
nandarionndr1 <leebet16@gmail.com>
2018-08-15 23:01:54 +0800

    crud for clients/

--- prime-sys/app/Http/Controllers/SupplierOrdersController.php
+++ prime-sys/app/Http/Controllers/SupplierOrdersController.php
@@ -12,7 +12,7 @@ class SupplierOrdersController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+
      * @return \Illuminate\Http\Response
      */
     public function index()
